c09aedc4c0d2360b9fd0f0cf6b195278e4724c37
Rahul Pradeep <realrahul02@gmail.com>
2022-06-16 22:13:52 +0530

    Test FIle Init
    Test Files added

---
+++ build/test.cb1e9afc.js
@@ -0,0 +1,2 @@
+const text=document.querySelector(".fancy"),strText=text.textContent,splitText=strText.split("");text.textContent="";for(let t=0;t<splitText.length;t++)text.innerHTML+="<span>"+splitText[t]+"</span>";let char=0,timer=setInterval(onTick,100);function onTick(){text.querySelectorAll("span")[char].classList.add("fade"),char++,char!=splitText.length||complete()}function complete(){clearInterval(timer),timer=null}
+//# sourceMappingURL=test.cb1e9afc.js.map
